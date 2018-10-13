d43e86cf98cf2a24e941c2252f244343c0235ed2
wilsonHodgson <whoffmeyer@hotmail.com>
2018-10-13 11:43:05 -0400

    Interpolated mesh without cached points, allowing for larger sample rate and smoother curves

--- Track.gd
+++ Track.gd
@@ -7,15 +7,26 @@ extends Path
 func drawRoad():
 	print(self.curve.get_baked_length())
 		# avoid using interpolate_baked, see results
-	for i in self.curve.get_baked_length():
-		var point_on_curve = self.curve.interpolate_baked(i)
-		var next_point_on_curve = self.curve.interpolate_baked(i+0.00001)
+	var num_samples = 160
+	var num_points = self.curve.get_point_count()
+	var num_subsamples = float(num_samples)/num_points
+	for i in num_points:
+		for j in num_subsamples:
+			if j != 0:
+				print(i+(1/num_subsamples*j))
+			else:
+				print(i)
+			var point_on_curve = self.curve.interpolate(i, 0)
+			var next_point_on_curve = self.curve.interpolate(i, 0.00001)
+			
+			if (j > 0) :
+				point_on_curve = self.curve.interpolate(i, 1/num_subsamples*j)
+				next_point_on_curve = self.curve.interpolate(i, (1/num_subsamples*j)+0.00001)
 		
-		var slope_of_curve = point_on_curve - next_point_on_curve
-		var perp_slope_of_curve = Vector3(0,1,0).cross(slope_of_curve.normalized())
-		$ImmediateGeometry.add_vertex(point_on_curve + 1*perp_slope_of_curve)
-		$ImmediateGeometry.add_vertex(point_on_curve + -1*perp_slope_of_curve)
-		print(self.curve.get_point_count())
+			var slope_of_curve = point_on_curve - next_point_on_curve
+			var perp_slope_of_curve = Vector3(0,1,0).cross(slope_of_curve.normalized())
+			$ImmediateGeometry.add_vertex(point_on_curve + 1*perp_slope_of_curve)
+			$ImmediateGeometry.add_vertex(point_on_curve + -1*perp_slope_of_curve)
 		
 
 func _ready():
