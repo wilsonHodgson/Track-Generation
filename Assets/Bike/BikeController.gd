3fd17e3424138d51a10794be21a4c8cc88a76cc8
Ben Russell <benfrussell@gmail.com>
2020-09-23 23:24:54 -0400

    Change methods for centering the bike body with the wheel

--- Assets/Bike/BikeController.gd
+++ Assets/Bike/BikeController.gd
@@ -4,53 +4,62 @@ onready var front_wheel = get_parent().find_node("BikeFrontWheel")
 onready var bike_body = get_parent().find_node("BikeBody")
 onready var wheel_joint = get_parent().find_node("FrontWheelJoint")
 
-var current_speed
+var current_speed = 0
+var max_speed = 5
 
-var friction_high = 1
+var friction_high = 1.5
 var friction_low = 0
 
 # Time to fully dampen turning after releasing the turn key in msec
 var turning_damp_time = 150 
 var _turning_damp_start
 
-# Spring and dampen the body back to center based on speed
-var speed_body_spring_ratio = 0.2
-var body_spring_max = 0.35
-var speed_body_dampen_ratio = 0.01
-var body_dampen_max = 0.015
+var body_center_time = 500
+var base_joint_angle = (30.0 / 180.0) * PI
+var _target_joint_angle = base_joint_angle
 
 var in_contact = false
+var accelerating = false
+var turning = false
 
 func _ready():
 	find_node("Camera").activate_camera()
 
 func _physics_process(delta):
-	_process_movement_input(delta)
-	
 	var direction = sign(front_wheel.transform.basis.z.dot(front_wheel.linear_velocity.normalized()))
-	current_speed = front_wheel.linear_velocity.length() * direction
-		
+	current_speed = (front_wheel.linear_velocity.length() * direction)
+	
+	_process_movement_input(delta)	
 	_update_bike_friction()
-	_update_bike_body_centering()
+	_update_bike_body_centering(delta)
 
 func _process_movement_input(delta):
-	if Input.is_action_just_pressed("bike_move_forward"):
-		bike_body.add_force(bike_body.transform.basis.z * 1000 * delta, Vector3(0,0,0))
+	accelerating = false
+	if Input.is_action_pressed("bike_move_forward"):
+		if current_speed < max_speed:
+			bike_body.add_force(bike_body.transform.basis.z * 150 * delta, Vector3(0,0,0))
+		if current_speed > 0:
+			accelerating = true
 			
-	if Input.is_action_just_pressed("bike_hop"):
+	if Input.is_action_just_pressed("bike_hop") and in_contact:
 		bike_body.add_force(bike_body.transform.basis.y * 1000 * delta, Vector3())
 		front_wheel.add_force(bike_body.transform.basis.y * 2000 * delta, Vector3())
 		
 	if Input.is_action_just_pressed("bike_rotate_left") or Input.is_action_just_pressed("bike_rotate_right"):
-		# Reset any turn dampening if we're starting to turn again
+		turning = true
 		front_wheel.angular_damp = -1
 		
 	if Input.is_action_pressed("bike_rotate_left"):
 		front_wheel.add_torque(Vector3(0, 2 * delta, 0))
+		bike_body.add_force(bike_body.transform.basis.x * (current_speed * 10) * delta, Vector3())
+		front_wheel.add_force(front_wheel.transform.basis.x * (current_speed * 10) * delta, Vector3())
 	elif Input.is_action_pressed("bike_rotate_right"):
 		front_wheel.add_torque(Vector3(0, -2 * delta, 0))
+		bike_body.add_force(bike_body.transform.basis.x * (current_speed * -10) * delta, Vector3())
+		front_wheel.add_force(front_wheel.transform.basis.x * (current_speed * -10) * delta, Vector3())
 	else:
 		if Input.is_action_just_released("bike_rotate_left") or Input.is_action_just_released("bike_rotate_right"):
+			turning = false
 			_turning_damp_start = OS.get_ticks_msec()
 		# Climb to fully dampened turning on a sine wave when no turning is pressed
 		if front_wheel.angular_damp < 1:
@@ -64,31 +73,28 @@ func _update_bike_friction():
 	var body_friction = friction_low + (_percent_velocity_sideways(bike_body) * friction_high)
 	bike_body.physics_material_override.friction = body_friction
 	print(body_friction)
-	
-func _update_bike_body_centering():
-	if current_speed > 0 and in_contact:
-		# Re-enable the spring
-		if not wheel_joint.get_flag_y(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_SPRING):
-			wheel_joint.set_flag_y(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_SPRING, true)
-			
-		# Spring the body on the angular y axis
-		var spring_power = min(current_speed * speed_body_spring_ratio, body_spring_max)
-		wheel_joint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_SPRING_STIFFNESS, spring_power)
 		
-		# Dampen excess body movement on the angular y axis
-		var dampen_power = min(current_speed * speed_body_dampen_ratio, body_dampen_max)
-		wheel_joint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_SPRING_DAMPING, dampen_power)
-	elif wheel_joint.get_flag_y(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_SPRING):
-		# Disable the spring if there's no forward speed or contact with other bodies
-		wheel_joint.set_flag_y(Generic6DOFJoint.FLAG_ENABLE_ANGULAR_SPRING, false)
+func _update_bike_body_centering(delta):
+	if accelerating and not turning:
+		_target_joint_angle = 0.0
+	else:
+		_target_joint_angle = stepify((30.0 / 180.0) * PI, 0.001)
+	
+	var cur_joint_angle = stepify(wheel_joint.get_param_y(Generic6DOFJoint.PARAM_ANGULAR_UPPER_LIMIT), 0.001)
+	if cur_joint_angle > _target_joint_angle:
+		# Percent of center_time elapsed in this tick * the total amount of change
+		var angle_change = (delta / (body_center_time / 1000.0)) * (base_joint_angle - _target_joint_angle)
+		var new_angle = max(cur_joint_angle - angle_change, 0)
+		wheel_joint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_UPPER_LIMIT, new_angle)
+		wheel_joint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_LOWER_LIMIT, new_angle * -1)
+	elif cur_joint_angle < _target_joint_angle:
+		wheel_joint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_UPPER_LIMIT, _target_joint_angle)
+		wheel_joint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_LOWER_LIMIT, _target_joint_angle * -1)
 
 func _percent_velocity_sideways(body):
-	if body.linear_velocity.length() < 0.01:
-		return 0
-	else:
-		# Calculate the dot product of the forward vector and the velocity vector
-		var dot_prod = body.transform.basis.z.normalized().dot(body.linear_velocity.normalized()) 
-		return sin((dot_prod - 1) * -0.5 * PI)
+	# Calculate the dot product of the forward vector and the velocity vector
+	var dot_prod = body.transform.basis.z.normalized().dot(body.linear_velocity.normalized()) 
+	return sin((dot_prod - 1) * -0.5 * PI)
 
 func _on_BikeFrontWheel_body_entered(body):
 	in_contact = true
