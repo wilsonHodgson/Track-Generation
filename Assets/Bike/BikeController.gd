ce571b1f9fffb57292425794c5d77b7bb5ec9f28
Benjamin Russell <benfrussell@gmail.com>
2020-10-18 14:13:20 -0400

    Add bike camera smoothing

--- Assets/Bike/BikeController.gd
+++ Assets/Bike/BikeController.gd
@@ -1,5 +1,7 @@
 extends Spatial
 
+signal started_accelerating
+
 onready var front_wheel = get_parent().find_node("BikeFrontWheel")
 onready var bike_body = get_parent().find_node("BikeBody")
 onready var wheel_joint = get_parent().find_node("FrontWheelJoint")
@@ -8,14 +10,15 @@ var current_speed = 0
 var max_speed = 5
 var _wheel_velocity_norm = Vector3()
 
-var friction_high = 2.0
+var friction_high = 3.0
 var friction_low = 0
 
 # Time to fully dampen turning after releasing the turn key in msec
 var turning_damp_time = 150 
 var _turning_damp_start
 
-var body_center_time = 500
+# Time for the body to be full centered on the front wheel when accelerating
+var body_center_time = 0.5
 var base_joint_angle = (30.0 / 180.0) * PI
 var _target_joint_angle = base_joint_angle
 
@@ -39,17 +42,20 @@ func _physics_process(delta):
 	_update_bike_body_centering(delta)
 
 func _process_movement_input(delta):
-	accelerating = false
 	if Input.is_action_pressed("bike_move_forward"):
+		if Input.is_action_just_pressed("bike_move_forward"):
+			accelerating = true
+			emit_signal("started_accelerating")
 		if current_speed < max_speed:
 			# Apply force towards the forward vector at max speed
-			var force_direction = (bike_body.global_transform.basis.z * max_speed) - bike_body.linear_velocity
-			print(current_speed)
-			bike_body.add_force(force_direction.normalized() * 150 * delta, Vector3())
-		if current_speed > 0:
-			accelerating = true
+			var force_direction = ((bike_body.global_transform.basis.z * max_speed) - bike_body.linear_velocity).normalized()
+			bike_body.add_force(force_direction * 150 * delta, Vector3())
+	else:
+		if Input.is_action_just_released("bike_move_forward"):
+			accelerating = false
 			
 	if Input.is_action_just_pressed("bike_hop") and in_contact:
+		print("jump")
 		bike_body.add_force(bike_body.transform.basis.y * 1000 * delta, Vector3())
 		front_wheel.add_force(bike_body.transform.basis.y * 2000 * delta, Vector3())
 		
@@ -82,19 +88,22 @@ func _update_bike_friction():
 	bike_body.physics_material_override.friction = body_friction
 		
 func _update_bike_body_centering(delta):
-	if accelerating and not turning:
+	if accelerating:
 		_target_joint_angle = 0.0
-	else:
+	elif turning:
+		# 30 degrees in radians, rounded
 		_target_joint_angle = stepify((30.0 / 180.0) * PI, 0.001)
 	
 	var cur_joint_angle = stepify(wheel_joint.get_param_y(Generic6DOFJoint.PARAM_ANGULAR_UPPER_LIMIT), 0.001)
 	if cur_joint_angle > _target_joint_angle:
+		# Gradually step the angle down if the current angle is greater than the target (i.e 30 -> 0deg)
 		# Percent of center_time elapsed in this tick * the total amount of change
-		var angle_change = (delta / (body_center_time / 1000.0)) * (base_joint_angle - _target_joint_angle)
+		var angle_change = (delta / body_center_time) * (base_joint_angle - _target_joint_angle)
 		var new_angle = max(cur_joint_angle - angle_change, 0)
 		wheel_joint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_UPPER_LIMIT, new_angle)
 		wheel_joint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_LOWER_LIMIT, new_angle * -1)
 	elif cur_joint_angle < _target_joint_angle:
+		# Set the angle in one step if the current angle is less than the target (i.e 0 -> 30deg)
 		wheel_joint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_UPPER_LIMIT, _target_joint_angle)
 		wheel_joint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_LOWER_LIMIT, _target_joint_angle * -1)
 	
