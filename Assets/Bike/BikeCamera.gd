ccf4e9a26048dac1b898d05e780e40bdfdcdd723
Benjamin Russell <benfrussell@gmail.com>
2020-10-22 14:05:40 -0400

    Add bike body roll rotations

--- Assets/Bike/BikeCamera.gd
+++ Assets/Bike/BikeCamera.gd
@@ -1,11 +1,9 @@
 extends Camera
 
+var camera_direction_delay = 2000
 var _activated = false
 var _sample_start_time = OS.get_ticks_msec()
 var _camera_dir_samples = []
-# Sample 1 in every X ticks
-var _camera_sample_rate = 1
-var _camera_sample_ticks = 0
 onready var _base_distance = Vector3(transform.origin.x, 0, transform.origin.z).length()
 onready var _base_height = transform.origin.y
 onready var _bike_wheel = get_parent().find_node("BikeFrontWheel")
@@ -22,14 +20,12 @@ func _process(delta):
 	_update_look_at()
 
 func _update_look_at():
-	_camera_sample_ticks += 1
 	var body_gtransform = _bike_body.global_transform
 	var wheel_gtransform = _bike_wheel.global_transform
 	
-	if _camera_sample_ticks % _camera_sample_rate == 0:
-		if OS.get_ticks_msec() - _sample_start_time > 3000:
-			_camera_dir_samples.pop_back()
-		_camera_dir_samples.insert(0, wheel_gtransform.basis.z)
+	if OS.get_ticks_msec() - _sample_start_time > camera_direction_delay:
+		_camera_dir_samples.pop_back()
+	_camera_dir_samples.insert(0, wheel_gtransform.basis.z)
 	
 	var look_at_target = wheel_gtransform.origin + (_average_vec_array(_camera_dir_samples) * 1)
 		
@@ -37,13 +33,12 @@ func _update_look_at():
 		wheel_gtransform.origin.x - _base_distance * wheel_gtransform.basis.z.x,
 		body_gtransform.origin.y + _base_height,
 		wheel_gtransform.origin.z - _base_distance * wheel_gtransform.basis.z.z)
-	look_at_from_position(look_at_position, look_at_target, wheel_gtransform.basis.y)
+	look_at_from_position(look_at_position, look_at_target, Vector3(0, 1, 0))
 	
 func _average_vec_array(a):
 	var array_sum = Vector3()
 	for item in a:
 		array_sum = item + array_sum
-	print(a.size())
 	return array_sum / a.size()
 	
 #func _on_Bike_started_accelerating():
--- Assets/Bike/BikeController.gd
+++ Assets/Bike/BikeController.gd
@@ -7,8 +7,9 @@ onready var bike_body = get_parent().find_node("BikeBody")
 onready var wheel_joint = get_parent().find_node("FrontWheelJoint")
 
 var current_speed = 0
+# Speed in units forward per second
 var max_speed = 5
-var _wheel_velocity_norm = Vector3()
+var max_turn_force = 20
 
 var friction_high = 3.0
 var friction_low = 0
@@ -17,9 +18,10 @@ var friction_low = 0
 var turning_damp_time = 150 
 var _turning_damp_start
 
-# Time for the body to be full centered on the front wheel when accelerating
-var body_center_time = 0.5
+# Distance that must be traveled for the body to centre itself
+var centre_body_travel = 1.5
 var base_joint_angle = (30.0 / 180.0) * PI
+var percent_centered = 0
 var _target_joint_angle = base_joint_angle
 
 var in_contact = false
@@ -30,9 +32,8 @@ func _ready():
 	find_node("Camera").activate_camera()
 
 func _physics_process(delta):
-	_wheel_velocity_norm = front_wheel.linear_velocity.normalized()
 	# Find the dot between the forward vector and the velocity vector to determine direction
-	var direction = sign(front_wheel.global_transform.basis.z.dot(_wheel_velocity_norm))
+	var direction = sign(front_wheel.global_transform.basis.z.dot(front_wheel.linear_velocity.normalized()))
 	# Project the velocity vector onto the forward vector at the same magnitude to determine the forward velocity
 	var forward_velocity = front_wheel.linear_velocity.project(front_wheel.global_transform.basis.z)
 	current_speed = forward_velocity.length() * direction
@@ -40,22 +41,26 @@ func _physics_process(delta):
 	_process_movement_input(delta)	
 	_update_bike_friction()
 	_update_bike_body_centering(delta)
+	_update_bike_roll(delta)
 
 func _process_movement_input(delta):
 	if Input.is_action_pressed("bike_move_forward"):
 		if Input.is_action_just_pressed("bike_move_forward"):
 			accelerating = true
 			emit_signal("started_accelerating")
-		if current_speed < max_speed:
+		if current_speed < max_speed and in_contact:
 			# Apply force towards the forward vector at max speed
 			var force_direction = ((bike_body.global_transform.basis.z * max_speed) - bike_body.linear_velocity).normalized()
+			bike_body.add_force(force_direction * 150 * delta, Vector3())
+		elif not in_contact:
+			# If accelerating but not in contact with the ground, continue to apply force to correct the forward direction
+			var force_direction = ((bike_body.global_transform.basis.z * current_speed) - bike_body.linear_velocity).normalized()
 			bike_body.add_force(force_direction * 150 * delta, Vector3())
 	else:
 		if Input.is_action_just_released("bike_move_forward"):
 			accelerating = false
 			
 	if Input.is_action_just_pressed("bike_hop") and in_contact:
-		print("jump")
 		bike_body.add_force(bike_body.transform.basis.y * 1000 * delta, Vector3())
 		front_wheel.add_force(bike_body.transform.basis.y * 2000 * delta, Vector3())
 		
@@ -65,12 +70,12 @@ func _process_movement_input(delta):
 		
 	if Input.is_action_pressed("bike_rotate_left"):
 		front_wheel.add_torque(Vector3(0, 2 * delta, 0))
-		bike_body.add_force(bike_body.transform.basis.x * (current_speed * 10) * delta, Vector3())
-		front_wheel.add_force(front_wheel.transform.basis.x * (current_speed * 10) * delta, Vector3())
+		bike_body.add_force(bike_body.transform.basis.x * (current_speed * max_turn_force) * delta, Vector3())
+		front_wheel.add_force(front_wheel.transform.basis.x * (current_speed * max_turn_force) * delta, Vector3())
 	elif Input.is_action_pressed("bike_rotate_right"):
 		front_wheel.add_torque(Vector3(0, -2 * delta, 0))
-		bike_body.add_force(bike_body.transform.basis.x * (current_speed * -10) * delta, Vector3())
-		front_wheel.add_force(front_wheel.transform.basis.x * (current_speed * -10) * delta, Vector3())
+		bike_body.add_force(bike_body.transform.basis.x * (current_speed * -max_turn_force) * delta, Vector3())
+		front_wheel.add_force(front_wheel.transform.basis.x * (current_speed * -max_turn_force) * delta, Vector3())
 	else:
 		if Input.is_action_just_released("bike_rotate_left") or Input.is_action_just_released("bike_rotate_right"):
 			turning = false
@@ -91,14 +96,15 @@ func _update_bike_body_centering(delta):
 	if accelerating:
 		_target_joint_angle = 0.0
 	elif turning:
-		# 30 degrees in radians, rounded
-		_target_joint_angle = stepify((30.0 / 180.0) * PI, 0.001)
+		_target_joint_angle = base_joint_angle
 	
 	var cur_joint_angle = stepify(wheel_joint.get_param_y(Generic6DOFJoint.PARAM_ANGULAR_UPPER_LIMIT), 0.001)
+	percent_centered = max(1 - (cur_joint_angle / base_joint_angle), 0)
 	if cur_joint_angle > _target_joint_angle:
 		# Gradually step the angle down if the current angle is greater than the target (i.e 30 -> 0deg)
-		# Percent of center_time elapsed in this tick * the total amount of change
-		var angle_change = (delta / body_center_time) * (base_joint_angle - _target_joint_angle)
+		# Percent of centre_body_travel traveled in this tick * the total angle to traverse
+		var distance_traveled_in_frame = max(current_speed * delta, 0)
+		var angle_change = (distance_traveled_in_frame / centre_body_travel) * (base_joint_angle - _target_joint_angle)
 		var new_angle = max(cur_joint_angle - angle_change, 0)
 		wheel_joint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_UPPER_LIMIT, new_angle)
 		wheel_joint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_LOWER_LIMIT, new_angle * -1)
@@ -106,6 +112,13 @@ func _update_bike_body_centering(delta):
 		# Set the angle in one step if the current angle is less than the target (i.e 0 -> 30deg)
 		wheel_joint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_UPPER_LIMIT, _target_joint_angle)
 		wheel_joint.set_param_y(Generic6DOFJoint.PARAM_ANGULAR_LOWER_LIMIT, _target_joint_angle * -1)
+		
+func _update_bike_roll(delta):
+	var forward_velocity_normal = bike_body.transform.basis.z.cross(bike_body.linear_velocity).y
+	var roll = min(abs(forward_velocity_normal), 2) * sign(forward_velocity_normal) * (current_speed / max_speed) * -1
+	wheel_joint.set_param_z(Generic6DOFJoint.PARAM_ANGULAR_UPPER_LIMIT, roll)
+	wheel_joint.set_param_z(Generic6DOFJoint.PARAM_ANGULAR_LOWER_LIMIT, roll)
+	
 	
 func _percent_velocity_sideways(body):
 	# Calculate the dot product of the forward vector and the velocity vector
